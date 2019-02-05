e8fa5e11259cc97e38d643c030f0856fac0f3bed
ceecurvin <ccurvi1@lsu.edu>
2019-02-05 18:54:38 +0000

    Feat; creating a handler for 404 Not Found error

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,8 @@ Starting an API
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
+from werkzeug.exceptions import NotFound
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -16,6 +18,14 @@ def close(content):
     handling teardowns
     """
     storage.close()
+
+@app.errorhandler(NotFound)
+def not_found(error):
+    """
+    Returns a 404 status code
+    """
+    return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", threaded=True)
