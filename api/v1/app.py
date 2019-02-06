bfb75b885b25d2f09c829abc6a0f0a242b0973aa
ceecurvin <ccurvi1@lsu.edu>
2019-02-06 23:32:35 +0000

    Fix; used getenv instead of environ

--- api/v1/app.py
+++ api/v1/app.py
@@ -35,8 +35,8 @@ def not_found(error):
 if __name__ == "__main__":
     host = '0.0.0.0'
     port = 5000
-    if not getenv('HBNB_API_HOST'):
+    if getenv('HBNB_API_HOST'):
         host = getenv('HBNB_API_HOST')
-    if not getenv('HBNB_API_PORT'):
+    if getenv('HBNB_API_PORT'):
         port = int(getenv('HBNB_API_PORT'))
     app.run(host=host, port=port, threaded=True)
