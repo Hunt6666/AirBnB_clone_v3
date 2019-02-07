c10d9dc20b3dbcbd0af1fa648c153825394859a2
Hunt6666 <489@holbertonschool.com>
2019-02-07 15:33:24 -0800

    fixing cascade

--- models/place.py
+++ models/place.py
@@ -23,11 +23,9 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('cities.id',
-                                                ondelete="CASCADE"),
+        city_id = Column(String(60), ForeignKey('cities.id'),
                          nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id',
-                                                ondelete="CASCADE"),
+        user_id = Column(String(60), ForeignKey('users.id'),
                          nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
@@ -37,7 +35,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place")
+        reviews = relationship("Review", cascade="all")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  cascade="all",
                                  viewonly=False)
--- models/user.py
+++ models/user.py
@@ -16,10 +16,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place",
-                              cascade="all", backref="user")
-        reviews = relationship("Review",
-                               cascade="all", backref="user")
+        places = relationship("Place", cascade="all")
+        reviews = relationship("Review", cascade="all")
     else:
         email = ""
         password = ""
